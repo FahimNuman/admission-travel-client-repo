1bacfec67e13efa18cdddcf6135f006649e69659
Md.Abdullah Al Numan Fahim <fahiomnuman87@gmail.com>
2022-11-10 10:27:08 +0600

    set redme file

--- src/components/Pages/Review/ReviewForm.js
+++ src/components/Pages/Review/ReviewForm.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 
 const ReviewForm = ({ review, handleDelete, handleStatusUpdate }) => {
     const { _id, displayName, email, status, service } = review;
